e69fbfb465cf22038d9b4daed88fe59839c54229
Luis Boscan <luife642@gmail.com>
2016-10-28 23:53:56 -0400

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Countdown = React.createClass({
+
+ render: function() {
+  return (
+   <p>Countdown.jsx</p>
+  );
+ }
+
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Timer = React.createClass({
+
+ render: function() {
+  return (
+   <p>Timer.jsx</p>
+  );
+ }
+
+});
+
+module.exports = Timer;
